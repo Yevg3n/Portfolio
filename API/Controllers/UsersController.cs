bf6101715620c218fd9f110e652e55aec84ecf46
Yevgeny <Yevgeny.Lavrov@yandex.com>
2023-05-23 20:55:52 -0400

    Create simple DB and user api

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class UsersController
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return users;
+        }
+    }
+}
