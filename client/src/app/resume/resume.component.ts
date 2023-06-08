45b494b7f6973c0ec1711e96c14073326f154d81
Yevgeny <Yevgeny.Lavrov@yandex.com>
2023-06-08 10:33:41 -0400

    Populate resume page

--- client/src/app/resume/resume.component.ts
+++ client/src/app/resume/resume.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent {
+export class ResumeComponent implements OnInit {
+  
+  constructor(private renderer: Renderer2) { }
 
+  ngOnInit(): void {
+    this.addExternalStylesheet('https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css');
+  }
+
+  addExternalStylesheet(url: string): void {
+    const link = this.renderer.createElement('link');
+    this.renderer.setAttribute(link, 'rel', 'stylesheet');
+    this.renderer.setAttribute(link, 'href', url);
+    this.renderer.appendChild(document.head, link);
+  }
 }
